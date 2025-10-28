798116d05b4b199d226a45d27ca443d98ffa9c04
Aryan Singh <aryan.singh.2024@computing.smu.edu.sg>
2025-10-28 01:00:51 +0000

    ex1

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -1,7 +1,9 @@
 <script>
     //import component
+    import SoundIcon from './SoundIcon.vue'
     
     export default { 
+        components: { SoundIcon },
         data() {
             return {
                 soundLevel: 0
@@ -12,10 +14,11 @@
 
 <template>
 
-   Sound level
-    <input type="number" min="0" max="3" v-model="soundLevel"> 
+    <div style="display:flex; align-items:center; gap:8px;">
+    Sound level
+    <input type="number" min="0" max="3" v-model.number="soundLevel"> 
     <!-- TODO: Add Code Here to make use of the component -->
-    
-
+    <SoundIcon :level="soundLevel" />
+  </div>
 </template>
 
